c7e21615058d0ca97c913034a140338e1be9b959
Fascka <Fasckax@gmail.com>
2017-10-18 19:32:14 -0300

    Added Loading Scene

---
+++ ClusterWaveServer/ClusterWaveServer/Scenes/LoadingScene.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClusterWaveServer.Scenes
+{
+    class LoadingScene
+    {
+        public LoadingScene() : base()
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+
+
+        public override void OnPacket(Lidgren.Network.NetIncomingMessage msg)
+        {
+
+        }
+    }
+}
